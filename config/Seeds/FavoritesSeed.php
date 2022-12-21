cc028f1a129ac441064c07c58a05467bbdda2704
ryuring <ryuring55@gmail.com>
2022-12-21 07:06:26 +0000

    prepare release

---
+++ config/Seeds/FavoritesSeed.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractSeed;
+
+/**
+ * Favorites seed.
+ */
+class FavoritesSeed extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeds is available here:
+     * https://book.cakephp.org/phinx/0/en/seeding.html
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'id' => '1',
+                'user_id' => '1',
+                'name' => 'クレジット',
+                'url' => 'javascript:$.bcCredit.show();',
+                'sort' => '1',
+                'created' => '2020-12-03 14:41:39',
+                'modified' => '2020-12-03 14:41:39',
+            ]
+        ];
+
+        $table = $this->table('favorites');
+        $table->insert($data)->save();
+    }
+}
