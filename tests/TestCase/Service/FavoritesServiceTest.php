2444b3edc0d22055103ceef73f13c1144dd26e0e
ryuring <ryuring55@gmail.com>
2024-10-10 10:32:38 +0000

    basre-coreが依存する他のパッケージのバージョン番号を固定化

--- tests/TestCase/Service/FavoritesServiceTest.php
+++ tests/TestCase/Service/FavoritesServiceTest.php
@@ -76,11 +76,12 @@ class FavoritesServiceTest extends BcTestCase
      */
     public function testGet(): void
     {
+        $this->expectException("Cake\Datasource\Exception\RecordNotFoundException");
+        $result = $this->FavoritesService->get(0);
+        $this->assertEmpty($result);
+
         $result = $this->FavoritesService->get(1);
         $this->assertEquals("固定ページ管理", $result->name);
-
-        $this->expectException('Cake\Datasource\Exception\RecordNotFoundException');
-        $result = $this->FavoritesService->get(0);
     }
 
     /**
